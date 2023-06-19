ae3fb4aef7cefa8f015bc5c43a9e58d035b4eed7
abdulazizatGitHub <abdulazizk1430@gmail.com>
2023-06-19 12:03:29 +0500

    Update Customer.js

--- backend/Routes/Customer.js
+++ backend/Routes/Customer.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { addCustomer, getCustomer } from '../Controllers/customerController.js';
 const router = express.Router();
 
-router.post("/add", addCustomer);
+router.post("/", addCustomer);
 // router.post("/SignUp/Google", addGoogleCustomer);
 
 router.post("/get", getCustomer);
